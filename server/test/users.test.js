4e8c059797496c7aa35c93bae79cb4ff36fa5a66
daniele24134 <capanodaniele@outlook.it>
2022-04-28 12:37:06 +0200

    create user test skeleton

---
+++ server/test/users.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const should = require('chai').should();
+
+// describe('TEST stupido', function () {
+//   it('should work', function (){
+//     'foo0'.should.have.lengthOf(3);
+//   });
+// })
+
+describe('createUser', function () {});
+
+describe('getGroups', function () {});
+
+describe('addGroup', function () {});
+
+describe('findUser', function () {});
