5c0d9a5ae1ebdd073aaf1ceb60bbdeb45fbbf7a0
daniele24134 <capanodaniele@outlook.it>
2022-04-28 12:23:29 +0200

    install server and client

---
+++ server/test/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// connect to mongoDb
+mongoose.connect('mongodb://localhost/groupay_test');
+
+mongoose.connection.once('open', function () {
+  console.log('Connection to test databse');
+
+}).on('error', function (error) {
+  console.log('TestDB connection error', error);
+});
+
