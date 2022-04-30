0aa68ab738fabaf077bbc868434986364510ad5a
daniele24134 <capanodaniele@outlook.it>
2022-04-30 12:37:48 +0200

    refactor: App navigation

--- client/groupay-client/src/components/App.js
+++ client/groupay-client/src/components/App.js
@@ -3,29 +3,42 @@ import { AuthProvider } from '../context/AuthContext';
 import Signup from './Signup';
 import Login from './Login';
 import Dashboard from './Dashboard';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import GroupPage from './GroupPage';
+import { useAuth } from '../context/AuthContext';
+import { Spinner } from 'react-bootstrap';
 
+function AppNavigation () {
+  const { currentUser } = useAuth();
+
+  const appNotLogged = (
+    <>
+      <Route exact path='/' element={<Spinner />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />}/>
+    </>
+  );
+
+  const appLogged = (
+    <>
+      <Route exact path='/' element={<Dashboard />} />
+      <Route path="/group/:groupName" element={<GroupPage />} />
+    </>
+  );
+
+  return (
+    <Routes>
+      {currentUser ? appLogged : appNotLogged }
+    </Routes>
+  )
+}
 
 function App() {
 
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route exact path='/' element={<PrivateRoute>
-            <Dashboard/>
-          </PrivateRoute>}/>
-          <Route path='/signup' element={<PublicRoute>
-            <Signup/>
-          </PublicRoute>}/>
-          <Route path='/login' element={<PublicRoute>
-            <Login/>
-          </PublicRoute>}/>
-          <Route path="/group/:groupName" element={<GroupPage />} />
-        </Routes>
+        <AppNavigation />
       </AuthProvider>
     </Router>
   );
